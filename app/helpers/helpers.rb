8576325baae5a4e618e553a5208cb9a8f40f9293
anupama <anupamagadde@yahoo.com>
2020-02-03 21:30:59 -0600

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,18 @@
 class Helpers
+    def self.current_user(session_hash)
+
+          @user = User.find_by(id: session_hash[:user_id])
+          
+           @user
+    end
+
+    def self.is_logged_in?(session_hash)
+        # if session_hash[:user_id]
+        #     true
+        # else
+        #     false
+        # end
+        !!session_hash[:user_id]
+
+    end
 end
