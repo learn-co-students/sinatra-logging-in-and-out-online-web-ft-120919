e58229f913dbdda5274943cb0949b67d0d11e68f
allison_johnson <allison.cuttler@gmail.com>
2020-02-04 19:50:52 -0800

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+  def self.current_user(hash)
+    id = hash[:user_id]
+    User.find(id)
+  end #self.current_user
+
+  def self.is_logged_in?(hash)
+    #binding.pry
+    #hash.keys.include?(:user_id)
+    !! hash[:user_id]
+  end #self.is_logged_in?
+  
 end
