475b1aa37447b38eb9c3177554ddd095cfe57df3
Scott Hoisington <hoyzington@gmail.com>
2020-02-06 14:56:38 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
-class Helpers
+class Helpers < ActiveRecord::Base
+  
+  def self.current_user(session_hash)
+    User.all.find_by_id(session_hash[:user_id])
+  end
+  
+  def self.is_logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
+  
 end
