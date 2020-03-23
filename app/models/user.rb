ed665efd62ec37de7092f40bb0ef0f58f61ba152
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-03-23 19:03:58 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessor :username, :password, :balance
-  def initialize(username, password, balance)
-    @username = username
-    @password = password
-    @balance = balance 
-  end 
+  # attr_accessor :username, :password, :balance
+  # def initialize(username=nil, password=nil, balance=nil)
+  #   @username = username
+  #   @password = password 
+  #   @balance = balance 
+  # end 
 end 
 
